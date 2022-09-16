ffdc8066c6b0c93cbe7a924f5e11858828b3bbbb
daddaz <89880145+daddaz@users.noreply.github.com>
2022-09-16 14:34:13 -0500

    second push

---
+++ scripts/ts/dynamic-dom/header.js
@@ -0,0 +1,18 @@
+export function header() {
+
+window.onscroll = function() {myFunction()}; 
+
+var header = document.getElementById("main-menu-container")
+
+var sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset > sticky) { 
+    header.classList.add("sticky");
+} else {
+   header.classList.remove("sticky");
+}
+
+}
+
+}
